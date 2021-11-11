b475c25045fed046ceadd7efd9237c5c83acf6d0
adonai-luque <akluqueprieto@gmail.com>
2021-11-11 03:55:54 -0500

    get_locations method optimized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,19 @@ class ApplicationController < ActionController::Base
 
   private
   def get_locations
-    next_url = "https://rickandmortyapi.com/api/location"
+    ids = (1..126).map(&:to_s).join(',')
+    puts ids
+    url = "https://rickandmortyapi.com/api/location/#{ids}"
+    data = JSON.parse(HTTP.get(url).body)
     locations = {}
-    until next_url == nil
-      data = JSON.parse(HTTP.get(next_url).body)
-      next_url = data["info"]["next"]
-      data["results"].each { |location| locations[location["id"]] = location["name"] }
-    end
+    data.each { |location| locations[location["id"]] = location["name"] }
+    # next_url = "https://rickandmortyapi.com/api/location"
+    # locations = {}
+    # until next_url == nil
+    #   data = JSON.parse(HTTP.get(next_url).body)
+    #   next_url = data["info"]["next"]
+    #   data["results"].each { |location| locations[location["id"]] = location["name"] }
+    # end
     return locations
   end
 
