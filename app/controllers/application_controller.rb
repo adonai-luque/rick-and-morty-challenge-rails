edd52f198e18d3439a1e7df35e2448574a06b12c
adonai-luque <akluqueprieto@gmail.com>
2021-11-11 02:20:23 -0500

    get_locations method added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,32 @@ require "json"
 class ApplicationController < ActionController::Base
 
   def index
+    start = Time.now
     @rick_and_morty = {
-      "locations" => [],
+      "locations" => get_locations,
       "episodes" => [],
       "characters" => []
     }
+    finish = Time.now
+    puts(finish - start)
     render template: "layouts/application"
   end
 
   private
+  def get_locations
+    next_url = "https://rickandmortyapi.com/api/location"
+    locations = {}
+    until next_url == nil
+      data = JSON.parse(HTTP.get(next_url).body)
+      next_url = data["info"]["next"]
+      data["results"].each { |location| locations[location["id"]] = location["name"] }
+    end
+    return locations
+  end
+
+  def get_episodes
+  end
+
+  def get_characters
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rick_and_morty_challenge_rb_production"
+  # config.active_job.queue_name_prefix = "rick_and_morty_challenge_rails_production"
 
   config.action_mailer.perform_caching = false
 
